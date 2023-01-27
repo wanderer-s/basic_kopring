eadf32576ec84fd188b61d0a9280302fdb2c736c
SEOP <ows0871@gmail.com>
2023-01-28 00:43:52 +0900

    kotlin spring init

---
+++ src/main/kotlin/com/basic/BagicApplication.kt
@@ -0,0 +1,11 @@
+package com.basic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BagicApplication
+
+fun main(args: Array<String>) {
+    runApplication<BagicApplication>(*args)
+}
