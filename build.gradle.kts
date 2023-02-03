62575b04c32c81f200ea1fedf6d92c3c6f441a3f
SEOP <ows0871@gmail.com>
2023-02-03 17:28:41 +0900

    암호화 종속성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,11 @@ plugins {
     kotlin("plugin.jpa") version "1.7.22"
 }
 
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperClass")
+}
+
 group = "com.basic"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
@@ -21,10 +26,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.mockk:mockk:1.13.4")
+    implementation("com.password4j:password4j:1.6.3")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.0")
 }
 
 tasks.withType<KotlinCompile> {
@@ -34,6 +43,6 @@ tasks.withType<KotlinCompile> {
     }
 }
 
-tasks.withType<Test> {
+tasks.withType<Test>() {
     useJUnitPlatform()
 }
